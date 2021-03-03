4205a2d954a2eb0f49896695a4e9cba2eba499ed
Denis Zhigun <zhigundv@gmail.com>
2021-03-03 22:10:03 +0300

    Update build.gradle junit4

--- src/test/java/ru/netology/domain/CashbackHackerTest.java
+++ src/test/java/ru/netology/domain/CashbackHackerTest.java
@@ -1,49 +1,49 @@
 package ru.netology.domain;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-class CashbackHackerTest {
+
+public class CashbackHackerTest {
     public CashbackHacker cashbackHacker = new CashbackHacker();
 
     @Test
     void shouldCashBackSum900() {
         int actual = cashbackHacker.remain(900);
         int expected = 100;
-        assertEquals(actual,expected);
+        assertEquals(expected,actual);
     }
     @Test
     void shouldCashBackSum100() {
         int actual = cashbackHacker.remain(100);
         int expected = 900;
-        assertEquals(actual,expected);
+        assertEquals(expected,actual);
     }
     @Test
     void shouldCashBackSum5000() {
-        int actual = cashbackHacker.remain(5000);  // Тест падает
+        int actual = cashbackHacker.remain(5000);
         int expected = 0;
-        assertEquals(actual,expected);
+        assertEquals(expected,actual);
     }
     @Test
     void shouldCashBackSum1000() {
-        int actual = cashbackHacker.remain(1000); // Тест падает
+        int actual = cashbackHacker.remain(1000);// тест падает
         int expected = 0;
-        assertEquals(actual,expected);
+        assertEquals(expected,actual);
     }
     @Test
     void shouldCashBackSum1100() {
         int actual = cashbackHacker.remain(1100);
         int expected = 900;
-        assertEquals(actual,expected);
+        assertEquals(expected,actual);
     }
     @Test
     void shouldCashBackSum5500() {
         int actual = cashbackHacker.remain(5500);
         int expected = 500;
-        assertEquals(actual,expected);
+        assertEquals(expected,actual);
     }
-
 
 
 }

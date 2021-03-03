ca01f031ca3c019af05a3f542ce9d66e0f45da75
Denis Zhigun <zhigundv@gmail.com>
2021-03-03 21:33:29 +0300

    Initial commit

---
+++ src/test/java/ru/netology/domain/CashbackHackerTest.java
@@ -0,0 +1,49 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackerTest {
+    public CashbackHacker cashbackHacker = new CashbackHacker();
+
+    @Test
+    void shouldCashBackSum900() {
+        int actual = cashbackHacker.remain(900);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCashBackSum100() {
+        int actual = cashbackHacker.remain(100);
+        int expected = 900;
+        assertEquals(expected,actual);
+    }
+    @Test
+    void shouldCashBackSum5000() {
+        int actual = cashbackHacker.remain(5000);  // Тест падает
+        int expected = 0;
+        assertEquals(expected,actual);
+    }
+    @Test
+    void shouldCashBackSum1000() {
+        int actual = cashbackHacker.remain(1000); // Тест падает
+        int expected = 0;
+        assertEquals(expected,actual);
+    }
+    @Test
+    void shouldCashBackSum1100() {
+        int actual = cashbackHacker.remain(1100);
+        int expected = 900;
+        assertEquals(expected,actual);
+    }
+    @Test
+    void shouldCashBackSum5500() {
+        int actual = cashbackHacker.remain(5500);
+        int expected = 500;
+        assertEquals(expected,actual);
+    }
+
+
+
+}

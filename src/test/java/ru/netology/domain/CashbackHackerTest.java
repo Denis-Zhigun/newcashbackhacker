e90441735e03c08f33ef463df83b6a6ce151470e
Denis Zhigun <zhigundv@gmail.com>
2021-03-06 10:37:39 +0300

    Added public in test

--- src/test/java/ru/netology/domain/CashbackHackerTest.java
+++ src/test/java/ru/netology/domain/CashbackHackerTest.java
@@ -9,37 +9,37 @@ public class CashbackHackerTest {
     public CashbackHacker cashbackHacker = new CashbackHacker();
 
     @Test
-    void shouldCashBackSum900() {
+    public void shouldCashBackSum900() {
         int actual = cashbackHacker.remain(900);
         int expected = 100;
         assertEquals(expected,actual);
     }
     @Test
-    void shouldCashBackSum100() {
+    public void shouldCashBackSum100() {
         int actual = cashbackHacker.remain(100);
         int expected = 900;
         assertEquals(expected,actual);
     }
     @Test
-    void shouldCashBackSum5000() {
+    public void shouldCashBackSum5000() {
         int actual = cashbackHacker.remain(5000);
         int expected = 0;
         assertEquals(expected,actual);
     }
     @Test
-    void shouldCashBackSum1000() {
+    public void shouldCashBackSum1000() {
         int actual = cashbackHacker.remain(1000);// тест падает
         int expected = 0;
         assertEquals(expected,actual);
     }
     @Test
-    void shouldCashBackSum1100() {
+    public void shouldCashBackSum1100() {
         int actual = cashbackHacker.remain(1100);
         int expected = 900;
         assertEquals(expected,actual);
     }
     @Test
-    void shouldCashBackSum5500() {
+    public void shouldCashBackSum5500() {
         int actual = cashbackHacker.remain(5500);
         int expected = 500;
         assertEquals(expected,actual);
